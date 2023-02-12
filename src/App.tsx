c10436bd05ee85f6081a21099f479d96d7ad8428
HartJN <jameshart04@gmail.com>
2023-02-12 23:00:31 +1300

    setup page props

--- src/App.tsx
+++ src/App.tsx
@@ -9,11 +9,14 @@ import ServicesPage from './pages/Services/ServicesPage'
 import PortfolioPage from './pages/Portfolio/PortfolioPage'
 import ContactPage from './pages/Contact/ContactPage'
 import {
+  aboutSections,
   footerNavLinksLeft,
   footerNavLinksRight,
   footerSocialIcons,
   mobileNavLinks,
   navLinks,
+  portfolioItems,
+  services,
 } from './data/index'
 
 function App() {
@@ -22,9 +25,18 @@ function App() {
       <NavBar navLinks={navLinks} mobileNavLinks={mobileNavLinks} />
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/portfolio" element={<PortfolioPage />} />
+        <Route
+          path="/about"
+          element={<AboutPage aboutSections={aboutSections} />}
+        />
+        <Route
+          path="/services"
+          element={<ServicesPage services={services} />}
+        />
+        <Route
+          path="/portfolio"
+          element={<PortfolioPage portfolioItems={portfolioItems} />}
+        />
         <Route path="/contact" element={<ContactPage />} />
       </Routes>
       <Footer
