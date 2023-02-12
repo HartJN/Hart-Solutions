21276a63d36d28359c7e49ec9e16d50576e7eeb9
HartJN <jameshart04@gmail.com>
2023-02-12 22:12:56 +1300

    add page data

---
+++ src/data/constants.ts
@@ -0,0 +1,100 @@
+// Footer
+
+export const footerSocialIcons = [
+  {
+    name: 'Facebook',
+    icon: 'src/assets/icon-facebook.svg',
+    link: 'https://www.facebook.com/',
+  },
+  {
+    name: 'Instagram',
+    icon: 'src/assets/icon-instagram.svg',
+    link: 'https://www.instagram.com/',
+  },
+  {
+    name: 'Twitter',
+    icon: 'src/assets/icon-twitter.svg',
+    link: 'https://twitter.com/',
+  },
+  {
+    name: 'Pinterest',
+    icon: 'src/assets/icon-pinterest.svg',
+    link: 'https://www.pinterest.com/',
+  },
+]
+
+export const footerNavLinksLeft = [
+  {
+    name: 'Home',
+    link: '/',
+  },
+  {
+    name: 'Services',
+    link: '/services',
+  },
+  {
+    name: 'About Us',
+    link: '/about',
+  },
+  {
+    name: 'Portfolio',
+    link: '/portfolio',
+  },
+]
+
+export const footerNavLinksRight = [
+  {
+    name: 'Contact Us',
+    link: '/contact',
+  },
+  {
+    name: 'Privacy Policy',
+    link: '/',
+  },
+]
+
+// Features
+
+export const features = [
+  {
+    id: 1,
+    title: 'Customized Solutions',
+    description:
+      "We don't believe in one-size-fits-all solutions. Instead, we work closely with our clients to understand their unique needs and design custom software solutions that meet those needs. Our goal is to help businesses achieve their goals and grow.",
+  },
+  {
+    id: 2,
+    title: 'Experienced Team',
+    description:
+      "Our team of developers and business consultants have years of experience delivering high-quality software solutions. We're experts in a wide range of technologies and always up-to-date with the latest trends and best practices.",
+  },
+  {
+    id: 3,
+    title: 'Results-Driven Approach',
+    description:
+      "We believe that technology should drive real business results. That's why we focus on delivering solutions that are designed to help our clients achieve their goals and grow. Our success is measured by the success of our clients.",
+  },
+]
+
+// Testimonials
+
+export const testimonials = [
+  {
+    id: 1,
+    name: 'Anisha Li',
+    text: "Hart Solutions was instrumental in helping us launch our new e-commerce platform. Their team was knowledgeable, efficient, and always a pleasure to work with. We appreciate their attention to detail and their commitment to delivering a quality product. We've already seen a significant increase in sales since launching the new platform and we couldn't be happier with the results.",
+    image: 'src/assets/avatar-anisha.png',
+  },
+  {
+    id: 2,
+    name: 'Ali Bravo',
+    text: "We had been struggling to find a software solution that could help us streamline our business processes, but after working with Hart Solutions, we finally have a system that works for us. Their team was able to understand our needs and design a custom solution that has made a big impact on our bottom line. We're grateful for their help and would highly recommend them to any business looking for software solutions.",
+    image: 'src/assets/avatar-ali.png',
+  },
+  {
+    id: 3,
+    name: 'Richard Watts',
+    text: 'Working with Hart Solutions was an excellent experience. Their team was professional, responsive, and always went above and beyond to make sure we were satisfied with the final product. They took the time to understand our needs and delivered a custom software solution that exceeded our expectations. We highly recommend them to anyone looking for a partner to help with their software needs.',
+    image: 'src/assets/avatar-richard.png',
+  },
+]
