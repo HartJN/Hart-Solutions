678da7d77b42c671cea55ae7b36c8b05d56b7074
HartJN <jameshart04@gmail.com>
2023-02-13 00:32:55 +1300

    style: temp change height

--- src/pages/Portfolio/PortfolioPage.tsx
+++ src/pages/Portfolio/PortfolioPage.tsx
@@ -11,7 +11,7 @@ interface Props {
 const Portfolio: React.FC<Props> = (props: Props) => {
   const { portfolioItems } = props
   return (
-    <div className="container mx-auto px-6 my-32 w-fit">
+    <div className="container mx-auto px-6 my-32 w-fit h-screen">
       <h1 className="text-3xl font-bold  mb-6 text-white">Our Portfolio</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {portfolioItems.map((item, index) => (
